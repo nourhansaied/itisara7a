86a89387b641a91fb47977bfa795a15cced367a7
nourhansaied <nourhan>
2023-10-12 10:52:21 +0300

    handl packae

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import express from 'express'
 import connection from './db/connection.js'
 import userRoutes from './modules/user/user.routes.js'
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 connection()
 app.use(express.json())
 
@@ -12,5 +12,6 @@ app.use(userRoutes);
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
